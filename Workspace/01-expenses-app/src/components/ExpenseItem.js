d7ddb2a69482171a22800c82941b604f84d3b84f
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-10 12:00:37 +0530

    Adding Basic CSS Styling

--- Workspace/01-expenses-app/src/components/ExpenseItem.js
+++ Workspace/01-expenses-app/src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
+// make the React nuild process aware that this .css should be used for this component
+import './ExpenseItem.css';
+
 // A component in React is just a JavaScript function
 function ExpenseItem() {
   // JSX - must return JSX code with only one root element.
   // Inside of that root element, we can have many adjacent elements
   return (
-    <div>
+    <div className='expense-item'>
       <div>Aug 10th 2021</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Order Food</h2>
-        <div>$100</div>
+        <div className='expense-item__price'>$100</div>
       </div>
     </div>
   );
