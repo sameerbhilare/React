7e4f1f8bbc82517c2279b4accbe9ae28d3621173
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-10 12:14:46 +0530

    Outputting Dynamic Data & Working with Expressions in JSX

--- Workspace/01-expenses-app/src/components/ExpenseItem.js
+++ Workspace/01-expenses-app/src/components/ExpenseItem.js
@@ -3,14 +3,18 @@ import './ExpenseItem.css';
 
 // A component in React is just a JavaScript function
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 9, 10);
+  const expenseTitle = 'Order Food';
+  const expenseAmount = 100;
+
   // JSX - must return JSX code with only one root element.
   // Inside of that root element, we can have many adjacent elements
   return (
     <div className='expense-item'>
-      <div>Aug 10th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Order Food</h2>
-        <div className='expense-item__price'>$100</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
