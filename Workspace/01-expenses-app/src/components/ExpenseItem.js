c8fa2835bceb2866926583d85e8fef9894ee2996
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-10 13:21:47 +0530

    Passing Data via "props"

--- Workspace/01-expenses-app/src/components/ExpenseItem.js
+++ Workspace/01-expenses-app/src/components/ExpenseItem.js
@@ -2,19 +2,19 @@
 import './ExpenseItem.css';
 
 // A component in React is just a JavaScript function
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 9, 10);
-  const expenseTitle = 'Order Food';
-  const expenseAmount = 100;
-
+/* React will pass a single object to our component function which will hold all passed-in parameters
+   with key as the name of the attribute and value as its value.
+   You can name it anything, but typically it is named as 'props'
+*/
+function ExpenseItem(props) {
   // JSX - must return JSX code with only one root element.
   // Inside of that root element, we can have many adjacent elements
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
