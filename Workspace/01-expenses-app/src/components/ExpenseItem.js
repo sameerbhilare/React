3c96ad75d292aa6eef0bf0c3292b6ed7e3a2cfbe
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-10 15:06:51 +0530

    Adding "normal" JavaScript Logic to Components

--- Workspace/01-expenses-app/src/components/ExpenseItem.js
+++ Workspace/01-expenses-app/src/components/ExpenseItem.js
@@ -7,11 +7,19 @@ import './ExpenseItem.css';
    You can name it anything, but typically it is named as 'props'
 */
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   // JSX - must return JSX code with only one root element.
   // Inside of that root element, we can have many adjacent elements
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
