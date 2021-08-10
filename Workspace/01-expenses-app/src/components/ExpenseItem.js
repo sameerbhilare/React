3707f6a036c0a5ec8fa782f481a2e66a320aa831
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-10 15:20:12 +0530

    Splitting Components Into Multiple Components

--- Workspace/01-expenses-app/src/components/ExpenseItem.js
+++ Workspace/01-expenses-app/src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 // make the React nuild process aware that this .css should be used for this component
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 // A component in React is just a JavaScript function
@@ -7,19 +8,12 @@ import './ExpenseItem.css';
    You can name it anything, but typically it is named as 'props'
 */
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   // JSX - must return JSX code with only one root element.
   // Inside of that root element, we can have many adjacent elements
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      {/* using nested custom component */}
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
