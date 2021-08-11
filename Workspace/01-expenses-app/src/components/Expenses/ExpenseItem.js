3af02140b50c91e88cacdb58dfc6f62f3941607d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-11 13:23:58 +0530

    Working with "State"

--- Workspace/01-expenses-app/src/components/Expenses/ExpenseItem.js
+++ Workspace/01-expenses-app/src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,11 @@
 // below import of 'React' is optional in latest versions of React.
 // It is used under the hood when we use JSX code
-import React from 'react';
+// 'React' is default import
+/* 'useState' function allows us to define values as state
+    where changes to these values should reflect in the component function being called again
+   'useState' is a React Hook. React Hooks start with the word "use" in their name, 
+    and all these hooks must only be called DIRECTLY inside of React component functions. (not inside nested function of component function) */
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
@@ -13,8 +18,20 @@ import './ExpenseItem.css';
    You can name it anything, but typically it is named as 'props'
 */
 const ExpenseItem = (props) => {
+  // invoking react hook
+  // useState always returns an array of exactly 2 elements.
+  // Name of the 'state' and a function to change value of that property
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem component function execuated!');
+
   // button click handler
   const clickHandler = () => {
+    // we cannot use title = 'Updated!', we haveto use setTitle function because
+    // calling setTitle() will not only update value of 'title' BUT ALSO it will execute this component function again!
+    setTitle('Updated!');
+    // IMP - At this point, the updated title will NOT be available bcz setTitle will 'schedule' the update, but eventually it will get updated.
+    //console.log(title);
+
     console.log('clicked!');
   };
 
@@ -25,7 +42,7 @@ const ExpenseItem = (props) => {
       {/* using nested custom component */}
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
         <button onClick={clickHandler}>Change Title</button>
       </div>
