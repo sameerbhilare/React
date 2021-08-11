0d5315fd52e9a92c63bb7f078fc9595b7936a44b
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-11 16:58:56 +0530

    Handling Form Submission

--- Workspace/01-expenses-app/src/components/NewExpense/ExpenseForm.js
+++ Workspace/01-expenses-app/src/components/NewExpense/ExpenseForm.js
@@ -80,8 +80,21 @@ const ExpenseForm = () => {
     });*/
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault(); // to prevent page submission
+
+    // combine State data
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
