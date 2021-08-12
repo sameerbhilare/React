3dcba0372205cc59885c7ed0b090f3e94f609ca7
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-12 18:50:34 +0530

    Adding a Users List Component

--- Workspace/03-users-app/src/App.js
+++ Workspace/03-users-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const users = [{ name: 'Sameer', age: 33 }];
   return (
     <div>
       <AddUser />
+      <UserList users={users} />
     </div>
   );
 }
---
+++ Workspace/03-users-app/src/components/Users/UserList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../UI/Card';
+
+import styles from './UserList.module.css'; // using CSS Modules
+
+const UserList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.name}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
