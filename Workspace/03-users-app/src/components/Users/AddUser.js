617515c6adfcbc8588bfc6d23d70081a644fcd43
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-12 19:15:05 +0530

    Adding The "ErrorModal" Component

--- Workspace/03-users-app/src/components/Users/AddUser.js
+++ Workspace/03-users-app/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 
 import styles from './AddUser.module.css'; // CSS Module
 
@@ -39,17 +40,25 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler} />
+    <div>
+      <ErrorModal title='Error occured' message='Message' />
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input
+            id='username'
+            type='text'
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
 
-        <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler} />
+          <label htmlFor='age'>Age (Years)</label>
+          <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler} />
 
-        <Button type='submit'>Add User</Button>
-      </form>
-    </Card>
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
