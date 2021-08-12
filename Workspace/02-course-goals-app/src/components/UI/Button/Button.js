5ced7d15df8dbde940678e5370b485386d29a996
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-12 16:41:28 +0530

    Using CSS Modules

--- Workspace/02-course-goals-app/src/components/UI/Button/Button.js
+++ Workspace/02-course-goals-app/src/components/UI/Button/Button.js
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import React from 'react';
+//import styled from 'styled-components';
+
+/*
+  This is basically a signal to the underlying React compilation process
+  to transform to code so that CSS Modules work.
+  The build process under the hood just takes those CSS classes and a CSS file
+  and basically changes those class names to be unique
+  so that those styles are 'scoped' to a component we import this file into.
+  We then access those CSS classes from the imported file as properties on the imported 'styles' object.
+*/
+import styles from './Button.module.css';
 
 //import './Button.css';
 
@@ -7,6 +18,7 @@ import styled from 'styled-components';
 // By default all the styles in .css files are available across the application,
 // which might cause some conflicts andundesired behavior
 // 'styled-components' package allows us to apply styles only to given component.
+/*
 const Button = styled.button`
   width: 100%;
   font: inherit;
@@ -32,6 +44,7 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+*/
 
 // const Button = props => {
 //   return (
@@ -40,5 +53,14 @@ const Button = styled.button`
 //     </button>
 //   );
 // };
+
+const Button = (props) => {
+  // access CSS classes from the imported file as properties on the imported 'styles' object.
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
