9caaf56c4619ede34f05124659ca57cb2b55c3e3
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-13 19:20:55 +0530

    Understanding useEffect

--- Workspace/04-dummy-auth-app/src/components/Login/Login.js
+++ Workspace/04-dummy-auth-app/src/components/Login/Login.js
@@ -11,6 +11,16 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // useEffect without dependencies so it will run for every component run cycle
+  useEffect(() => {
+    console.log('Effect running!');
+  });
+
+  // useEffect with empty dependencies so it will run only once when the component is loaded for the first time
+  useEffect(() => {
+    console.log('Effect running once only !');
+  }, []);
+
   /*
     Handling (Side) Effects with useEffect React hook.
     Whenever you have an action that should be executed in response to some other action, that is a "side effect".
@@ -19,9 +29,33 @@ const Login = (props) => {
 
     We want to call setFormIsValid() only if the email or password fields are changed (side effect of user entering some data)
   */
-  useEffect(() => {
-    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
-  }, [enteredEmail, enteredPassword]);
+  useEffect(
+    /*
+      IMPLEMENTING DEBOUNCE TIME - to avoid setTimeout function execution for each key stroke
+      The idea is to wait for 500 ms. 
+      If user doesn't type anthing for 500 ms, then only setTimeout function executes. Otherwise the timeout will be cleared in the cleanup function below
+    */
+
+    // side-effect function (1st arg)
+    () => {
+      const identifier = setTimeout(() => {
+        console.log('checking form validity');
+        setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+      }, 500);
+
+      // returning 'Cleanup function'
+      // This returned function will run as a cleanup process before useEffect executes this function the next time.
+      // In addition, this cleanup function will run whenever
+      // the component you're specifying the effect in unmounts from the DOM. So whenever the component is removed.
+      // it will not run for the first side-effect execution (obviously).
+      return () => {
+        console.log('CLEANUP');
+        clearTimeout(identifier);
+      };
+    },
+    // dependencies (2nd arg)
+    [enteredEmail, enteredPassword]
+  );
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
