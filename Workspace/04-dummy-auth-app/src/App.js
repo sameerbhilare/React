d4222ead3376f7ef0b1f5cd38b76061943deec9d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-13 18:22:06 +0530

    Using the useEffect() Hook - to manage authentication info

--- Workspace/04-dummy-auth-app/src/App.js
+++ Workspace/04-dummy-auth-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,27 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  /*
+    Handling (Side) Effects with useEffect React hook.
+    useEffect() will get called whenever this App component is evaluated/(reevaluated due any state changes)
+    However the function inside it (1st argument) will execute ONLY if the dependencies (2nd arg) are changed
+  */
+  useEffect(() => {
+    const storedUserLoginInfo = localStorage.getItem('isLoggedIn');
+    if (storedUserLoginInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
