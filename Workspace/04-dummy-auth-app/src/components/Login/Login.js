87376a9bf4a61d1ebfa1d7c79d2f4bd356e50caf
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-13 18:38:43 +0530

    useEffect and dependencies - for common validation

--- Workspace/04-dummy-auth-app/src/components/Login/Login.js
+++ Workspace/04-dummy-auth-app/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,28 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  /*
+    Handling (Side) Effects with useEffect React hook.
+    Whenever you have an action that should be executed in response to some other action, that is a "side effect".
+    useEffect() will get called whenever this App component is evaluated/(reevaluated due any state changes)
+    However the function inside it (1st argument) will execute ONLY if the dependencies (2nd arg) are changed
+
+    We want to call setFormIsValid() only if the email or password fields are changed (side effect of user entering some data)
+  */
+  useEffect(() => {
+    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    setFormIsValid(event.target.value.includes('@') && enteredPassword.trim().length > 6);
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    setFormIsValid(event.target.value.trim().length > 6 && enteredEmail.includes('@'));
   };
 
   const validateEmailHandler = () => {
@@ -43,36 +51,28 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
+        <div className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''}`}>
+          <label htmlFor='email'>E-Mail</label>
           <input
-            type="email"
-            id="email"
+            type='email'
+            id='email'
             value={enteredEmail}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
-        <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
+        <div className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''}`}>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             value={enteredPassword}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
         </div>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type='submit' className={classes.btn} disabled={!formIsValid}>
             Login
           </Button>
         </div>
