ad4d33b9835dec7993deeea6d1b902d4b3cd30d3
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-14 16:27:19 +0530

    Adding the "Cart" Button Component

--- Workspace/07-food-order-app/src/components/Layout/Header.js
+++ Workspace/07-food-order-app/src/components/Layout/Header.js
@@ -5,13 +5,14 @@ import styles from './Header.module.css';
 
 // importing the image and using it
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Card</button>
+        <HeaderCartButton>Card</HeaderCartButton>
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
