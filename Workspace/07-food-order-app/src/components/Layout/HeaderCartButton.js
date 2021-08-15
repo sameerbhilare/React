744017ea118f31173568e4b05f1a934879c16103
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-15 16:13:20 +0530

    Using the useEffect Hook to add/remove css animation class on btn clicks

--- Workspace/07-food-order-app/src/components/Layout/HeaderCartButton.js
+++ Workspace/07-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart.context';
 
 import CartIcon from '../Cart/CartIcon';
@@ -6,11 +6,36 @@ import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => currNumber + item.amount, 0);
+  const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
+
+  const numberOfCartItems = items.reduce((currNumber, item) => currNumber + item.amount, 0);
+
+  const btnClasses = `${styles.button} ${isBtnHighlighted ? styles.bump : ''}`;
+
+  // using useEffect to show bump animation on every cart item change
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    // set to tru so that 'bump' css class will be added
+    setIsBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      // set to tru so that 'bump' css class will be removed after 300 ms (after animation plays)
+      setIsBtnHighlighted(false);
+    }, 300);
+
+    // cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
