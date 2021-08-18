d520da7fc2530386512044c89e059b56e3c4b5de
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-18 19:06:06 +0530

    Adding a First Class-based Component

--- Workspace/09-class-based-components/src/components/User.js
+++ Workspace/09-class-based-components/src/components/User.js
@@ -2,14 +2,26 @@ import { Component } from 'react';
 
 import classes from './User.module.css';
 
+/* 
+  Need to extend the class with React's Component class.
+  This Component class adds important functionality to make your class work as a component.
+  And it also adds a couple of important properties For example, the 'props' property 
+  which can be accessed as 'this.props....'
+
+  Class-based components can work together with functional components.
+*/
 class User extends Component {
   componentWillUnmount() {
     console.log('User will unmount!');
   }
 
+  /*
+    The method name has to be 'render()' in class-based components.
+    render() method in a class is your equivalent to the return statement in a functional component
+  */
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
-  } 
+  }
 }
 
 // const User = (props) => {
