1bfb07f45fd1cefffd224046076b60c748e3ff24
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-19 17:01:26 +0530

    Configuring Custom Hooks

--- Workspace/11-custom-hooks-demo/src/components/BackwardCounter.js
+++ Workspace/11-custom-hooks-demo/src/components/BackwardCounter.js
@@ -1,17 +1,20 @@
-import { useState, useEffect } from 'react';
+import useCounter from '../hooks/use-counter';
 
 import Card from './Card';
 
 const BackwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  /*
+    Using Custom hook =>
+    The state used in the useCounter custom hook will be tried to this component.
+    If we use a custom hook in multiple components, every component will receive its own state.
+    
+    So just because we use a custom hook does not mean that we share state or effects across components.
+    Instead for every component the custom hook is executed again.
+    And every component instance then receives its own state. 
+    
+    So it's just a "logic which has shared" not the concrete state.
+  */
+  const counter = useCounter(false);
 
   return <Card>{counter}</Card>;
 };
--- Workspace/11-custom-hooks-demo/src/hooks/use-counter.js
+++ Workspace/11-custom-hooks-demo/src/hooks/use-counter.js
@@ -5,17 +5,19 @@ import { useEffect, useState } from 'react';
     The word 'use' signals to react that it will be a custom hook 
     and it gives react the guarantee that you will use that function by respecting the "rules of hooks"
     So you will use this custom hook function just as you use to built-in hooks.
+
+    Custom Hook function can accept parameters just like built-in hooks.
 */
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => (forwards ? prevCounter + 1 : prevCounter - 1));
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]); // 'forwards' is external dependencies
 
   // we can return anything we want
   return counter;
