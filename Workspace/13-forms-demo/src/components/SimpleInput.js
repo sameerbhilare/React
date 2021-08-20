e53fd595a0ee5dba528895a91065653f34fd9b16
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-20 18:16:31 +0530

    Managing The Overall Form Validity

--- Workspace/13-forms-demo/src/components/SimpleInput.js
+++ Workspace/13-forms-demo/src/components/SimpleInput.js
@@ -11,6 +11,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  // checking overall form validity
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -53,7 +59,7 @@ const SimpleInput = (props) => {
         {nameInputInvalid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
