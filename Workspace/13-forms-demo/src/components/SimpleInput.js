d34bcdf027108c59f402c9d05c6820bf48fc9a7e
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-20 17:26:38 +0530

    Using state and onChange event on input

--- Workspace/13-forms-demo/src/components/SimpleInput.js
+++ Workspace/13-forms-demo/src/components/SimpleInput.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // imp
+    console.log(enteredName);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
