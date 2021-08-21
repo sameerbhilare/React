07bee9a81ab3fff0120ba455b8f41a862a8c30a2
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-21 16:58:57 +0530

    Reading Form Values via useRef

--- Workspace/14-food-order-app-with-forms-and-http/src/components/Cart/Checkout.js
+++ Workspace/14-food-order-app-with-forms-and-http/src/components/Cart/Checkout.js
@@ -1,28 +1,41 @@
 // importing the styles
+import { useRef } from 'react';
 import styles from './Checkout.module.css';
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    console.log({ enteredName, enteredStreet, enteredPostalCode, enteredCity });
   };
 
   return (
     <form className={styles.form} onSubmit={confirmHandler}>
       <div className={styles.control}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
+        <input type='text' id='street' ref={streetInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
+        <input type='text' id='postal' ref={postalCodeInputRef} />
       </div>
       <div className={styles.control}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input type='text' id='city' ref={cityInputRef} />
       </div>
       <div className={styles.actions}>
         <button type='button' onClick={props.onCancel}>
