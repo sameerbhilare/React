1d2bb118e9abdd5c2d513c1d23ec5f5768927631
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-23 16:56:55 +0530

    Reading & Dispatching From Auth Slice

--- Workspace/16-redux-counter-example/src/App.js
+++ Workspace/16-redux-counter-example/src/App.js
@@ -1,12 +1,17 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); // 'auth' is key name used in configureStore -> reducer property in index.js
+
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </>
   );
--- Workspace/16-redux-counter-example/src/components/Header.js
+++ Workspace/16-redux-counter-example/src/components/Header.js
@@ -1,22 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store';
 import classes from './Header.module.css';
 
 const Header = () => {
+  // accessing the state Auth slice
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); // 'auth' is key name used in configureStore -> reducer property in index.js
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    // dispatchign actions to state Auth slice
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuthenticated && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
