7f6c32a1802423aa9580b6d73915e5f72071156d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-23 15:50:00 +0530

    Connecting Redux Toolkit State

--- Workspace/16-redux-counter-example/src/store/index.js
+++ Workspace/16-redux-counter-example/src/store/index.js
@@ -1,6 +1,5 @@
 // REDUX Logic
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
@@ -21,7 +20,7 @@ const initialState = {
     Each reducer method will be called for you by Redux, and they will receive the current state and action obj.
     It may not require action bcz these reducer methods will be called based on action itself
 */
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState: initialState,
   reducers: {
@@ -52,70 +51,17 @@ createSlice({
   },
 });
 
-/*
-    Reducer function - which will update the state in the store.
-    A reducer function is a standard JavaScript function, but it will it be called by the Redux library
-    and it will then always receive two inputs - the existing state and the action that was dispatched.
-    AND it must always return a new state object.
-
-    A reducer function should be a "pure function" which basically means that the same inputs 
-    always should produce exactly the same output. 
-    And there should be no side effects inside of that function. 
-    So you must not send a HTTP request or write something to local storage 
-    or fetch something from local storage there.
-
-    Instead, a reducer should really just be a function that takes the given inputs, 
-    which are provided by Redux and then produces the expected output, a new state object.
-
-    The objects which we return in the reducer will not be merged with the existing state.
-    They will overwrite the existing state. 
-    So we must always set all the other states when we update a piece of state.
-
-    **************** IMPORTANT **************** 
-    When working with Redux, Never mutate the existing state. 
-    This can lead to bugs, unpredictable behavior and it can make debugging your application harder as well. 
-    Instead, always override the state by returning a brand new state object.
-    And because objects and arrays are reference values in JavaScript, 
-    it's easy to accidentally override and change the existing state. So be careful !!!
-*/
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'increase') {
-    return {
-      counter: state.counter + action.amount, // using payload
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'decrement') {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-
-  return state;
-};
-
 // create a store
 /*
-    Store needs the Reducer function as an input
-    because the store needs to know which reducer is responsible for changing/manipulating this store.
-    As soon as store is created, the reducer function is run.
+    configureStore() is powerful, we can add multiple slices of a store with it.
 */
-const store = createStore(counterReducer); // createStore is from 'redux' package
+const store = configureStore({
+  // value to this 'reducer' key be reducer of a single slice
+  // or a map of reduers of multiple slices and
+  // behind the scenes configureStore will merge all those reducers into one big reducer.
+  reducer: counterSlice.reducer, // single slice
+
+  //reducer: {counter: counterSlice.reducer} // multiple slices.
+});
 
 export default store;
