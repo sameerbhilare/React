aa1bb27913e2ed835b66c8605e55b7b7c30e0729
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-23 11:01:42 +0530

    Providing the Store

--- Workspace/16-redux-counter-example/src/index.js
+++ Workspace/16-redux-counter-example/src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+    If the vast majority of your components (entire app) need access to the store,
+    you should typically provide on this highest level.
+    It will make sure that this component and its child components and their child components and so on 
+    will get access to the store
+*/
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
