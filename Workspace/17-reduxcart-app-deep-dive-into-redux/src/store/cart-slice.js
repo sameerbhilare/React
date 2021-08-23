2fc8ec150830c258881da4a0519c15c1992d3ca6
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-23 19:32:47 +0530

    Added Cart slice and related changes

--- Workspace/17-reduxcart-app-deep-dive-into-redux/src/store/cart-slice.js
+++ Workspace/17-reduxcart-app-deep-dive-into-redux/src/store/cart-slice.js
@@ -1,3 +1,53 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialCartState = {};
+const initialCartState = {
+  items: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialCartState,
+  reducers: {
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        /*
+            Redux Toolkit internally ensures that this will not manipulate the existing state 
+            but that it instead transforms this into an operation 
+            which updates the state in an immutable way. So we can use push here when working with Redux Toolkit.
+          */
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+
+    removeItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
