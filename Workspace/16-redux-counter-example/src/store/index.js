7bfab1337fe51d27438f1582e6c0c24ee438be52
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-23 15:40:47 +0530

    Adding State Slices

--- Workspace/16-redux-counter-example/src/store/index.js
+++ Workspace/16-redux-counter-example/src/store/index.js
@@ -1,10 +1,57 @@
 // REDUX Logic
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
+
+/*
+    createSlice is even more powerful than createReducer.
+    It takes an object as an argument.
+    With createSlice, we are preparing a slice of our global state.
+    When we have different pieces of state which are not directly related, 
+    e.g. authentication status and the counter status, we could create different slices
+    potentially also in different files to make our code maintainable.
+
+    Every slice needs a 'name' so an identifier of that piece of state.
+    'initialState' which is initial state of this slice 
+    and 'reducers' which is an object of all the reducers this slice needs.
+    Each reducer method will be called for you by Redux, and they will receive the current state and action obj.
+    It may not require action bcz these reducer methods will be called based on action itself
+*/
+createSlice({
+  name: 'counter',
+  initialState: initialState,
+  reducers: {
+    increment(state) {
+      // here it is allowed to 'mutate' the state bcz :) ->
+      /*
+            When using Redux toolkit and its functions like createSlice, 
+            we can't accidentally manipulate the existing state.
+            Because Redux toolkit internally uses another package, called Immer, 
+            which will automatically clone the existing state, create a new state object, 
+            keep all the state which we're not editing, and override the state 
+            which we are editing in an 'immutable' way.
+        */
+      state.counter++;
+    },
+
+    decrement(state) {
+      state.counter--;
+    },
+
+    increase(state, action) {
+      state.counter = state.counter + action.amount; // using payload
+    },
+
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 /*
     Reducer function - which will update the state in the store.
     A reducer function is a standard JavaScript function, but it will it be called by the Redux library
