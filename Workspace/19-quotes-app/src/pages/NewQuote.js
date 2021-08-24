a3ddf9bde046acb2247172868daf0762f06bba57
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-24 18:45:33 +0530

    Implementing Programmatic (Imperative) Navigation

--- Workspace/19-quotes-app/src/pages/NewQuote.js
+++ Workspace/19-quotes-app/src/pages/NewQuote.js
@@ -1,8 +1,22 @@
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  /*
+    PRPGRAMMATIC NAVIGATION -
+    useHistory hook allows us to change the browser history. So the history of pages we visited.
+    history.push() - pushes a new page on the stack of pages
+    history.replace() - replaces the current page.
+    The difference is that with push(), we can go back with the back button to the page we're coming from,
+    with replace we can't.
+  */
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    // pushes a new page on the stack of pages
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
