deb4f24b8ca11de8917de84791cf4006247cd239
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-24 16:49:25 +0530

    Working with Nested Routes

--- Workspace/18-react-router-demo/src/pages/Welcome.js
+++ Workspace/18-react-router-demo/src/pages/Welcome.js
@@ -1,6 +1,16 @@
+import { Route } from 'react-router-dom';
+
 // Regular component but used as a page based on URL, hence kept in pages folder
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      {/* Nested Routes. */}
+      <Route path='/welcome/new-user'>
+        <p>Welcome new user</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
