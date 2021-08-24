f7f176bce633c30cc843eeecfc22c8e29e4f194c
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-24 16:45:43 +0530

    Using "Switch" and "exact" For Configuring Routes

--- Workspace/18-react-router-demo/src/App.js
+++ Workspace/18-react-router-demo/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import ProductDetail from './pages/ProductDetail';
 import Products from './pages/Products';
@@ -14,22 +14,30 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        {/* Route is a component that allows us to define a certain path 
+        {/* With <Switch> component, only first matching route will be loaded.
+          This will also consider if a route has a property called 'exact'.
+         */}
+        <Switch>
+          {/* Route is a component that allows us to define a certain path 
           and then the React component that should be loaded when that path becomes active in the URL.
 
           Below Route component will make sure that this 'Welcome' component is only displayed on the screen 
           if our URL path is /welcome
+
+          By default (without <Switch>), the routes are not parsed such that only one of them is loaded at the same time.
+          But instead all routes that match (not exact match) the current path will be loaded.
         */}
-        <Route path='/welcome'>
-          <Welcome />
-        </Route>
-        <Route path='/products'>
-          <Products />
-        </Route>
-        {/* Dynamic Path */}
-        <Route path='/product-detail/:productId'>
-          <ProductDetail />
-        </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          {/* Dynamic Path */}
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- Workspace/18-react-router-demo/src/pages/Products.js
+++ Workspace/18-react-router-demo/src/pages/Products.js
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom';
+
 // Regular component but used as a page based on URL, hence kept in pages folder
 const Products = () => {
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
-        <li>A Book</li>
-        <li>A Carpet</li>
-        <li>An Online Course</li>
+        <li>
+          <Link to='/products/p1'>A Book</Link>
+        </li>
+        <li>
+          <Link to='/products/p2'>A Carpet</Link>
+        </li>
+        <li>
+          <Link to='/products/p3'>An Online Course</Link>
+        </li>
       </ul>
     </section>
   );
