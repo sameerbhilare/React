1a98f434185b980505ceb82d9e3ffeeaaebe8843
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-24 18:59:51 +0530

    Preventing Possibly Unwanted Route Transitions with the "Prompt" Component

--- Workspace/19-quotes-app/src/components/quotes/QuoteForm.js
+++ Workspace/19-quotes-app/src/components/quotes/QuoteForm.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { useRef } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+  const [isUserEntering, setIsUserEntering] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,28 +23,60 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishedEnteringFormHandler = () => {
+    /* IMP *********
+    Setting setIsUserEntering(false) inside of the submitFormHandler would NOT work
+    because that's a little bit too late. This state update (setIsUserEntering(false)) would not go through,
+    before we actually triggered a navigation action.
+    Because the navigation action will be triggered in on props.onAddQuote().
+    So that's all one synchronous process 
+    and since the state updates are "scheduled", the state update would not be processed before we try to navigate away.
+    That's why I'm using a separate function for that here which triggers when the button is clicked
+    and therefore before we actually handle the form submission.
+    */
+    setIsUserEntering(false);
+  };
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusedHandler = () => {
+    setIsUserEntering(true);
+  };
+
+  return (
+    <>
+      {/* 
+      Use Prompt component to prevent unwanted page transitions/navigations.
+      Prompt component will automatically watch if we navigate away from this page.
+      And if then a certain condition is met, it will show a warning before it allows us to leave. */}
+      <Prompt
+        when={isUserEntering}
+        message={(location) =>
+          'Are you sure you want to leave? All your entered data will be lost!'
+        }
+      />
+      <Card>
+        <form onFocus={formFocusedHandler} className={classes.form} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishedEnteringFormHandler} className='btn'>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 };
 
