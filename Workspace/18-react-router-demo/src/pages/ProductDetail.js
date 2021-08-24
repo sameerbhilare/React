da5a3732ff8afd40f4bbbf046f75ad5a401f25a5
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-24 16:34:21 +0530

    Extracting Route Params

--- Workspace/18-react-router-demo/src/pages/ProductDetail.js
+++ Workspace/18-react-router-demo/src/pages/ProductDetail.js
@@ -1,7 +1,16 @@
+import { useParams } from 'react-router-dom';
+
 const ProductDetail = () => {
+  /*
+        useParams returns an object where the keys are the dynamic segments leading to that page. 
+        e.g. productId in case of ourdomain.com/product-detail/:productId
+    */
+  const params = useParams();
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
