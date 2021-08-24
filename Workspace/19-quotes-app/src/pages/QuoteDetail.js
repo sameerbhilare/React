0037736d14fcb0f4d493c5dd34e10c4f65fc4723
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-24 17:54:02 +0530

    Practicing Nested Routes

--- Workspace/19-quotes-app/src/pages/QuoteDetail.js
+++ Workspace/19-quotes-app/src/pages/QuoteDetail.js
@@ -1,8 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { Route, useParams } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
   const params = useParams();
-  return <h1>Quote Details Page {params.quoteId}</h1>;
+  return (
+    <>
+      <h1>Quote Details Page</h1>
+      <p>{params.quoteId}</p>
+      {/* <Route path={`/quotes/${params.quoteId}/comments`} exact> */}
+      <Route path='/quotes/:quoteId/comments' exact>
+        <Comments />
+      </Route>
+    </>
+  );
 };
 
 export default QuoteDetail;
