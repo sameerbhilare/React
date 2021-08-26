2441ba04b7b80e4a9740b692941ce88469356ce8
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 10:42:07 +0530

    Adding nested pages

---
+++ Workspace/21-nextjs-course/pages/news/detail.js
@@ -0,0 +1,8 @@
+// this file will be served when request reaches ourdomain.com/news/detail
+// the name 'index.js' is a special name.
+
+const DetailPage = () => {
+  return <h1>The News Detail Page</h1>;
+};
+
+export default DetailPage;
---
+++ Workspace/21-nextjs-course/pages/news/index.js
@@ -0,0 +1,8 @@
+// this file will be served when request reaches ourdomain.com/news
+// the name 'index.js' is a special name.
+
+const NewsPage = () => {
+  return <h1>The News Page</h1>;
+};
+
+export default NewsPage;
