b29c9f0d027c52d88c89e3d4efe86fa1c2f37175
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 11:53:55 +0530

    The "_app.js" File & Layout Wrapper

--- Workspace/22-nextjs-meetups-app/pages/_app.js
+++ Workspace/22-nextjs-meetups-app/pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Layout from '../components/layout/Layout';
+import '../styles/globals.css';
 
+/*
+  "_app.js" is the root component of Next.js application.
+  Nextjs passes the Component and pageProps props to this root component.
+  'Component' is a prop that holds the actual page contents that should be rendered.
+  So it will be different whenever we switch a page and 
+  'pageProps' are specific props our pages might be getting.
+
+  So 'Component here in this _app.js file will in the end be the actual page content of our different pages.
+  And it will change whenever we navigate from page A to page B.
+
+  So whenever you have some component or some setting that affects all your pages e.g. layout,
+  you can utilize this _app.js file to easily add that without diving into dozens of files individually.
+*/
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
