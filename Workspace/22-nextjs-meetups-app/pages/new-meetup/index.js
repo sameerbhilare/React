d027e2de99001300110a02854bce56c56a343041
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 11:42:08 +0530

    Adding the New Meetup Form

--- Workspace/22-nextjs-meetups-app/pages/new-meetup/index.js
+++ Workspace/22-nextjs-meetups-app/pages/new-meetup/index.js
@@ -1,2 +1,14 @@
 // This file will be served when request reaches ourdomain.com/new-meetup
 // the name 'index.js' is a special name.
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetupPage;
