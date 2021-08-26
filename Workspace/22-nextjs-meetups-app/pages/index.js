6feb9f7a21396321ba260c81a72bb488a57e611d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 11:18:59 +0530

    Preparing the Project Pages

---
+++ Workspace/22-nextjs-meetups-app/pages/index.js
@@ -0,0 +1,2 @@
+// This file will be served when request reaches ourdomain.com/
+// the name 'index.js' is a special name.
---
+++ Workspace/22-nextjs-meetups-app/pages/new-meetup/index.js
@@ -0,0 +1,2 @@
+// This file will be served when request reaches ourdomain.com/new-meetup
+// the name 'index.js' is a special name.
