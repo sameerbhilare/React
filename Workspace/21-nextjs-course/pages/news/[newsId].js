05e60a3501629672a5b7b35980e5d0cddff8f039
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 10:51:20 +0530

    Extracting Dynamic Parameter Values

--- Workspace/21-nextjs-course/pages/news/[newsId].js
+++ Workspace/21-nextjs-course/pages/news/[newsId].js
@@ -2,8 +2,19 @@
 // [] in the file name tells nextjs that this will be dynamic page
 // so that it should be loaded for different values in your path.
 
+import { useRouter } from 'next/router';
+
 const DetailPage = () => {
-  return <h1>The News Detail Page</h1>;
+  /*
+    With nestjs' useRouter, we get access to methods for programatic nagivation
+    and also for getting values in the URL.
+  */
+  const router = useRouter(); // hook from next/router
+
+  // accessing the parameters
+  const newsId = router.query.newsId;
+
+  return <h1>The News Detail Page - {newsId}</h1>;
 };
 
 export default DetailPage;
