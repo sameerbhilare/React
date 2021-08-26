3b8b227f6c6730cac798bc85857bfd20bbcfb628
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 16:16:53 +0530

    Sending Http Requests To Our API Routes

--- Workspace/22-nextjs-meetups-app/pages/new-meetup/index.js
+++ Workspace/22-nextjs-meetups-app/pages/new-meetup/index.js
@@ -1,11 +1,27 @@
 // This file will be served when request reaches ourdomain.com/new-meetup
 // the name 'index.js' is a special name.
 
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
     console.log(enteredMeetupData);
+
+    // send request to API route which will be internal path as both frontend and backend are on same server
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
