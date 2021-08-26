54ef1f52b4f5fb18e2b4dff9edd5753cf448c802
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 13:16:27 +0530

    How Pre-rendering Works & Which Problem We Face

--- Workspace/22-nextjs-meetups-app/pages/index.js
+++ Workspace/22-nextjs-meetups-app/pages/index.js
@@ -1,6 +1,7 @@
 // This file will be served when request reaches ourdomain.com/
 // the name 'index.js' is a special name.
 
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -23,7 +24,24 @@ const DUMMY_MEETUPS = [
 ];
 
 const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  /* 
+  IMPORTANT - 
+  Here useEffect will execute the function after this HomePage component is loaded.
+  So for the first render cycle (which will be server side pre-rendered page) the loadedMeetups will be empty.
+  And those are fetched only in the 2nd render cycle as part of useEffect function execution.
+  So if you see the html page source, you will not see the loadedMeetups 
+  as those are fetched only after the component is rendered in the browser.
+  
+  From SEO point of view, this is a problem.
+  */
+  useEffect(() => {
+    // send http request and load data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 };
 
 export default HomePage;
