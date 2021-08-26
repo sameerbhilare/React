9c7df7432181b8caaa611dbe05b61ddfd4d96bb6
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-26 12:32:32 +0530

    Adding Custom Components & CSS Modules

--- Workspace/22-nextjs-meetups-app/pages/[meetupId]/index.js
+++ Workspace/22-nextjs-meetups-app/pages/[meetupId]/index.js
@@ -1,2 +1,18 @@
 // This file will be served when request reaches ourdomain.com/:meetupId
 // the name 'index.js' is a special name.
+// keep page components as lean as possible.
+
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
+      title='Our First Meetup'
+      address='Some street 5, City'
+      description='A meetup description'
+    />
+  );
+};
+
+export default MeetupDetailsPage;
