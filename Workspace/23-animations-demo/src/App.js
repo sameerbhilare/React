208fd84308593d706f68fb01877da7e418a39dc8
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-27 09:46:12 +0530

    Wrapping the Transition Component

--- Workspace/23-animations-demo/src/App.js
+++ Workspace/23-animations-demo/src/App.js
@@ -48,11 +48,7 @@ class App extends Component {
             ></div>
           )}
         </Transition>
-
-        <Transition in={this.state.modalIsOpen} timeout={300} mountOnEnter unmountOnExit>
-          {/* 'state' will be either of 'entering', 'entered', 'existing', 'exited' */}
-          {(state) => <Modal show={state} closed={this.closeModal} />}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
 
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className='Button' onClick={this.showModal}>
