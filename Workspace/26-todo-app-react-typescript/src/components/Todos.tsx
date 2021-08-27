3380d4ac72741ed6a876adea77717a9a946c4a70
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-08-27 15:12:17 +0530

    Added TodoItem

--- Workspace/26-todo-app-react-typescript/src/components/Todos.tsx
+++ Workspace/26-todo-app-react-typescript/src/components/Todos.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 /*
   React.FC is from @types/react. React.FC is a generic type.
@@ -19,7 +20,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text}/>
       ))}
     </ul>
   );
